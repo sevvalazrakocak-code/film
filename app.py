928a69cdb2a760af46683ab1457368e672217840
sevvalazrakocak-code <sevvalazrakocak@gmail.com>
2025-12-11 22:53:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# API Key'i ortam değişkeninden al (Güvenlik için en iyi pratik)
-API_KEY = os.environ.get("OMDB_API_KEY")
+
+API_KEY = "86a9f482"  
 
 @app.route("/", methods=["GET", "POST"])
 def index():
@@ -14,21 +14,22 @@ def index():
 
     if request.method == "POST":
         movie_name = request.form.get("movie_name")
-        if movie_name and API_KEY:
-            # OMDb API'ye istek atıyoruz
+        
+        if movie_name:
+            # OMDb API isteği
             url = f"http://www.omdbapi.com/?t={movie_name}&apikey={API_KEY}"
-            response = requests.get(url)
-            data = response.json()
+            try:
+                response = requests.get(url)
+                data = response.json()
 
-            if data.get("Response") == "True":
-                movie_data = data
-            else:
-                error = "Film bulunamadı veya API hatası!"
-        elif not API_KEY:
-            error = "API Key eksik! Lütfen ortam değişkeni olarak atayın."
-
+                if data.get("Response") == "True":
+                    movie_data = data
+                else:
+                    error = "Film bulunamadı! (İsmi doğru yazdınız mı?)"
+            except:
+                error = "API'ye ulaşılamadı, internet bağlantınızı kontrol edin."
+                
     return render_template("index.html", movie=movie_data, error=error)
 
 if __name__ == "__main__":
-    # Docker içinde dışarıya açılması için host='0.0.0.0' olmalı
     app.run(host="0.0.0.0", port=5000)
